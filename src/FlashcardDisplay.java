9d8e34e5a99a21852a7c6614b69645731cc9d23d
jingz <Henryz49@nycstudents.net>
2023-06-04 22:18:50 -0400

    second commit, first week done

---
+++ src/FlashcardDisplay.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class FlashcardDisplay {
+    private JPanel panelMain;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JLabel introductionText;
+    private JList list1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
